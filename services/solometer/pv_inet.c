4ca011058c3cd6fae4e0379de9d2c729a7b06fd0
Pidder <github@hartmann-peter.de>
2010-05-02 20:12:02 +0200

    Minor changes

--- services/solometer/pv_inet.c
+++ services/solometer/pv_inet.c
@@ -37,7 +37,7 @@ uint16_t spindex = 0;
 
 static char *HOSTNAME = "your.hostname.goes.here";
 static uip_conn_t *post_conn;
-static char *poststr1 = "POST /upload_file.php HTTP/1.0\r\n"
+static char *poststr1 = "POST /upload_data.php HTTP/1.0\r\n"
 		  "Host: your.hostname.goes.here\r\n"
 		  "Content-Type: multipart/form-data; boundary=bOunDaRy\r\nContent-Length: 160000\r\n\r\n";
 static char *poststr2 = "--bOunDaRy\r\nContent-Disposition: form-data; name=\"file\"; filename=\"";
